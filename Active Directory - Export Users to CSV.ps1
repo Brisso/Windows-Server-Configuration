e0fe5ffd19c54d402cc2b4874dc2c0c42c261202
Michael Brissenden <34114234+Brisso@users.noreply.github.com>
2018-08-24 12:31:17 +1000

    Active Directory - Export Users to CSV

---
+++ Active Directory - Export Users to CSV.ps1
@@ -0,0 +1,10 @@
+## Brissotech
+## Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+## Source - https://github.com/Brisso/Windows-Server-Setup
+
+## Description
+## Script designed to Export List of Users From AD into the Bulk Import Format.
+
+
+## Script Needs to be Updated Here
