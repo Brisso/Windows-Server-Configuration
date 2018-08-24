394faae2cd928ae71add30cfa5b44e6d2c0a4758
Michael Brissenden <34114234+Brisso@users.noreply.github.com>
2018-08-24 12:32:23 +1000

    Create Active Directory - Bulk Import Users.ps1

---
+++ Active Directory - Bulk Import Users.ps1
@@ -0,0 +1,10 @@
+## Brissotech
+## Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+## Source - https://github.com/Brisso/Windows-Server-Setup
+
+## Description
+## Script designed to Bulk import Users to AD from the Export CSV.
+
+
+## Script Needs to be Updated Here
